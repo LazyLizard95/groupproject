4c1b26cfb800b9a553da1bbb0a82228db321fa64
LazyLizard95 <51461957+LazyLizard95@users.noreply.github.com>
2021-11-02 17:51:27 -0400

    href
    testing href

--- script.js
+++ script.js
@@ -5,7 +5,18 @@ const testFetch  = fetch('http://www.boredapi.com/api/activity/')
   .then(function (response) {
     
 console.log(response);
-  })
+  }) 
 
+ const googleTest = fetch('https://www.googleapis.com/customsearch/v1?key=AIzaSyAKzE32n6Ad0lWIE3hOhOeYWyMbOoQ0PyE&cx=017576662512468239146:omuauf_lfve&q=lectures')
+ .then(function (gresponse) {
+   return gresponse.json();
+ }).then(function (gresponse) {
+   console.log(gresponse);
+ })
 
- 
+ function changeLink(){
+   document.getElementById("displayText").href = testest;
+   document.getElementById("displayText").innerText = googleTest;
+ }
+
+ changeLink();
