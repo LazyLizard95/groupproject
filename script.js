60f62236954ade1840894e751bc80373efe311f5
Hlaing Aung <betahba@gmail.com>
2021-11-02 17:47:52 -0400

    google fetch

--- script.js
+++ script.js
@@ -8,4 +8,14 @@ console.log(response);
   })
 
 
+  const testGoogle  = fetch('https://www.googleapis.com/customsearch/v1?key=AIzaSyAKzE32n6Ad0lWIE3hOhOeYWyMbOoQ0PyE&q=football&cx=6a0eab11f4f52b42f')
+  .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      
+  console.log(response);
+    })
+
+
  
