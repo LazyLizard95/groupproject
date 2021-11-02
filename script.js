5278bae0068730db7c17edf69879a74fbca301be
LazyLizard95 <51461957+LazyLizard95@users.noreply.github.com>
2021-11-02 18:20:09 -0400

    almost mvp
    can search links in console log.

--- script.js
+++ script.js
@@ -3,11 +3,12 @@ const testFetch  = fetch('http://www.boredapi.com/api/activity/')
     return response.json();
   })
   .then(function (response) {
-    
-console.log(response);
-  }) 
+    console.log(response);
+    activity = response.activity
+    console.log(activity);
+   
 
-  const testGoogle  = fetch('https://www.googleapis.com/customsearch/v1?key=AIzaSyAKzE32n6Ad0lWIE3hOhOeYWyMbOoQ0PyE&q=' +activity '&cx=6a0eab11f4f52b42f')
+  const testGoogle  = fetch('https://www.googleapis.com/customsearch/v1?key=AIzaSyAKzE32n6Ad0lWIE3hOhOeYWyMbOoQ0PyE&q=' +activity+ '&cx=6a0eab11f4f52b42f')
   .then(function (response) {
       return response.json();
     })
@@ -15,7 +16,7 @@ console.log(response);
       
   console.log(response);
     })
-
+  })
  function changeLink(){
    document.getElementById("displayText").href = "testest";
    document.getElementById("displayText").innerText = googleTest;
@@ -26,5 +27,21 @@ console.log(response);
 
   
 
-
+//assign a variable for generated activity.
+//append said variable to google search.
+//display activity. 
  
+
+
+
+
+
+
+
+
+
+
+//local past generated activity
+//points system
+//task completion streak
+//set options for type tasks checkboxes
