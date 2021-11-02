676dedf811bf2077f6d554499c785ede86088267
LazyLizard95 <51461957+LazyLizard95@users.noreply.github.com>
2021-11-02 18:01:09 -0400

    href link thing
    href

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const testFetch  = fetch('http://www.boredapi.com/api/activity/')
 console.log(response);
   }) 
 
-  const testGoogle  = fetch('https://www.googleapis.com/customsearch/v1?key=AIzaSyAKzE32n6Ad0lWIE3hOhOeYWyMbOoQ0PyE&q=football&cx=6a0eab11f4f52b42f')
+  const testGoogle  = fetch('https://www.googleapis.com/customsearch/v1?key=AIzaSyAKzE32n6Ad0lWIE3hOhOeYWyMbOoQ0PyE&q=' +activity '&cx=6a0eab11f4f52b42f')
   .then(function (response) {
       return response.json();
     })
@@ -17,7 +17,7 @@ console.log(response);
     })
 
  function changeLink(){
-   document.getElementById("displayText").href = testest;
+   document.getElementById("displayText").href = "testest";
    document.getElementById("displayText").innerText = googleTest;
  }
 
