98b49c204e59cdc7b7a8160b8fd4dec6a2276d78
LazyLizard95 <51461957+LazyLizard95@users.noreply.github.com>
2021-11-03 19:42:30 -0400

    added for loop
    trying to parse

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 let displayText = document.querySelector("#displayText").textContent;
 let testGoogle = "";
 let taskButton = document.querySelector("#generate");
+let taskContainer = document.querySelector("#contentContainer");
+
 
 taskButton.addEventListener("click", function(){
 
@@ -12,7 +14,7 @@ taskButton.addEventListener("click", function(){
       console.log(response);
       activity = response.activity
       console.log(activity);
-      document.getElementById("displayText").href = "testest";
+      
       document.getElementById("displayText").innerText = activity;
   
    testGoogle  = fetch('https://www.googleapis.com/customsearch/v1?key=AIzaSyAKzE32n6Ad0lWIE3hOhOeYWyMbOoQ0PyE&q=' +activity+ '&cx=6a0eab11f4f52b42f')
@@ -20,7 +22,11 @@ taskButton.addEventListener("click", function(){
         return response.json();
       })
       .then(function (response) {
+        for(i =0; i < response.items.length; i++) {
+          taskContainer.append(JSON.parse("response", response.items[i]));
+        }
         
+        // document.getElementById("displayText").href = response.items[0].link;
     console.log(response);
       })
     })
