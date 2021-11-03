c2d465441348f02af2f269ad86a8ccbe317a96d8
LazyLizard95 <51461957+LazyLizard95@users.noreply.github.com>
2021-11-03 19:30:51 -0400

    Added button to generate tasks
    added task generation button
    Tasks display on screen

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,34 @@
-const testFetch  = fetch('http://www.boredapi.com/api/activity/')
-.then(function (response) {
-    return response.json();
-  })
-  .then(function (response) {
-    console.log(response);
-    activity = response.activity
-    console.log(activity);
-   
+let displayText = document.querySelector("#displayText").textContent;
+let testGoogle = "";
+let taskButton = document.querySelector("#generate");
 
-  const testGoogle  = fetch('https://www.googleapis.com/customsearch/v1?key=AIzaSyAKzE32n6Ad0lWIE3hOhOeYWyMbOoQ0PyE&q=' +activity+ '&cx=6a0eab11f4f52b42f')
+taskButton.addEventListener("click", function(){
+
+   testFetch  = fetch('http://www.boredapi.com/api/activity/')
   .then(function (response) {
       return response.json();
     })
     .then(function (response) {
-      
-  console.log(response);
+      console.log(response);
+      activity = response.activity
+      console.log(activity);
+      document.getElementById("displayText").href = "testest";
+      document.getElementById("displayText").innerText = activity;
+  
+   testGoogle  = fetch('https://www.googleapis.com/customsearch/v1?key=AIzaSyAKzE32n6Ad0lWIE3hOhOeYWyMbOoQ0PyE&q=' +activity+ '&cx=6a0eab11f4f52b42f')
+    .then(function (response) {
+        return response.json();
+      })
+      .then(function (response) {
+        
+    console.log(response);
+      })
     })
-  })
- function changeLink(){
-   document.getElementById("displayText").href = "testest";
-   document.getElementById("displayText").innerText = googleTest;
- }
 
 
- changeLink();
+})
+
+ 
 
   
 
