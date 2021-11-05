5acda7c0e116e787b970582701f7a68a8779ecc4
LazyLizard95 <51461957+LazyLizard95@users.noreply.github.com>
2021-11-05 12:23:49 -0400

    using regex
    using a regular expression instead of an array so the search engine wont have commas

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,10 @@ let taskButton = document.querySelector("#generate");
 let taskContainer = document.querySelector("#contentContainer");
 
 
-taskButton.addEventListener("click", function(){
+taskButton.addEventListener("click", function () {
 
-   testFetch  = fetch('http://www.boredapi.com/api/activity/')
-  .then(function (response) {
+  testFetch = fetch('http://www.boredapi.com/api/activity/')
+    .then(function (response) {
       return response.json();
     })
     .then(function (response) {
@@ -16,47 +16,45 @@ taskButton.addEventListener("click", function(){
       console.log(activity);
       document.getElementById("displayText").innerText = activity;
       activity.toLowerCase(); //converts activity AFTER its been appended for user. 
-      let newStringArray = activity.split(' '); //converts activity string to an array
-      const bannedTerms = ['the', 'a', 'and', 'too', 'or', 'some'] //terms that are checked for within the array so they can be removed for better search results
+      newStringArray = activity.match(/\b(\w+)\b/g); //this is known as a regular expression or ReGex. Regular expression are a combination of characters that are short hand for parameters that can be matched. https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions/Cheatsheet
+      const bannedTerms = ['the', 'a', 'and', 'too', 'or', 'some', 'of'] //terms that are checked for within the array so they can be removed for better search results
       newStringArray = newStringArray.filter(e => bannedTerms.includes(e) === false); //e represents all the elements in a list, => returns the values, e => bannedTerms.includes(e) === false is saying, do not return the elements if they are included in banned terms
-      let cleanedSearch = newStringArray.toString(); //convert the array back to a string
-      
-      
-  
-   testGoogle  = fetch('https://www.googleapis.com/customsearch/v1?key=AIzaSyAMyF69ErI_ssDgFwmw2UFM8ia0cpRyRsM&q=' +cleanedSearch+ '&cx=6a0eab11f4f52b42f') //the cleaned search is included here
-    .then(function (response) {
-        return response.json();
-      })
-      .then(function (response) {
-        //debugger;
-        for(i =0; i < response.items.length; i++) {
-          var links = document.createElement("div");
-          //var aTag = document.createElement("a");
-          var linkItems = response.items[i].link;
-          var linkTitles = response.items[i].title;
-          links.innerHTML ='<br>' + '<a href=' +linkItems+ '>' + linkTitles;
-          taskContainer.append(links);
-          
-          // taskContainer.append(response.items[i].link);
-          console.log(response.items[i].link);
+      console.log(activity);
 
-        }
-        
-        // document.getElementById("displayText").href = response.items[0].link;
-    console.log(response);
-      })
+
+      testGoogle = fetch('https://www.googleapis.com/customsearch/v1?key=AIzaSyAMyF69ErI_ssDgFwmw2UFM8ia0cpRyRsM&q=' + activity + '&cx=6a0eab11f4f52b42f') //the activity is included here
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (response) {
+          //debugger;
+          for (i = 0; i < response.items.length; i++) {
+            var links = document.createElement("div");
+            var linkItems = response.items[i].link;
+            var linkTitles = response.items[i].title;
+            links.innerHTML = '<br>' + '<a href=' + linkItems + '>' + linkTitles;
+            taskContainer.append(links);
+
+
+            console.log(response.items[i].link);
+
+          }
+
+
+          console.log(response);
+        })
     })
 
 
 })
 
 
- 
-
-  
 
 
- 
+
+
+
+
 
 
 
