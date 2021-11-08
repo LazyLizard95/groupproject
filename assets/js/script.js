4d7805dc52961f5b81b62d0851a84944b2317631
LazyLizard95 <51461957+LazyLizard95@users.noreply.github.com>
2021-11-08 16:43:35 -0500

    Save function in almost ready state
    Currently saving null but it is targeting correct elements.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ let displayText = document.querySelector("#displayText").textContent;
 let testGoogle = "";
 let taskButton = document.querySelector("#generate");
 let taskContainer = document.querySelector("#contentContainer");
-
+let book = [];
 
 taskButton.addEventListener("click", function () {
 
@@ -17,7 +17,7 @@ taskButton.addEventListener("click", function () {
       document.getElementById("displayText").innerText = activity;
       activity.toLowerCase(); //converts activity AFTER its been appended for user. 
       let newStringArray = activity.split(' '); //converts activity string to an array
-      const bannedTerms = ['the', 'a', 'and', 'too', 'or', 'some', 'of'];//terms that are checked for within the array so they can be removed for better search results
+      const bannedTerms = ['the', 'a', 'and', 'too', 'or', 'some', 'of', 'an'];//terms that are checked for within the array so they can be removed for better search results
       newStringArray = newStringArray.filter(e => bannedTerms.includes(e) === false); //e represents all the elements in a list, => returns the values, e => bannedTerms.includes(e) === false is saying, do not return the elements if they are included in banned terms
       let cleanedSearch = newStringArray.toString(); //convert the array back to a string
       console.log(cleanedSearch);
@@ -28,39 +28,48 @@ taskButton.addEventListener("click", function () {
           return response.json();
         })
         .then(function (response) {
-          //debugger;
+          let contentBox = document.getElementsByClassName('contentContainer');
+          let contentBoxId = [];
+          for (i = 0, len = contentBox.length; i < len; i++){
+            if (contentBox[i].id != ''){
+              contentBoxId.push(contentBox[i].id);
+            }
+          }
+          console.log(contentBoxId);
           for (i = 0; i < response.items.length; i++) {
+            
             var links = document.createElement("div");
-            links.classList.add('container');
             var linkItems = response.items[i].link;
             var linkTitles = response.items[i].title;
             links.innerHTML = '<br>' + '<a href=' + linkItems + '>' + linkTitles;
-            taskContainer.append(links);
+            contentBox[i].append(links);
 
-
-            console.log(response.items[i].link);
-
+            
+           
+            
           }
 
-
-          console.log(response);
+          
+          
         })
     })
 
 
 })
 
+let saveMark = document.getElementById("save");
+console.log(saveMark);
+saveMark.addEventListener("click", function (e) { 
+  var link = e.target.nextElementSibling.getAttribute("href");
+      var title = e.target.nextElementSibling.text;
+      book.push({
+      link,
+      title,
+      });
+      localStorage.setItem("bookmarked", JSON.stringify(book));
+      console.log("works");
 
-
-
-
-
-
-
-
-
-
-
+    })
 
 
 
